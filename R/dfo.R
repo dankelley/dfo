5a7793faa19c9bd5cad776769eeb071d286388e6
dankelley <kelley.dan@gmail.com>
2021-02-07 10:30:35 -0400

    document package as a whole (close #2)

---
+++ R/dfo.R
@@ -0,0 +1,21 @@
+#' dfo: Handle Dept. Fisheries & Oceans Data Formats
+#'
+#' @description
+#'
+#' The `dfo` package provides functions for working with
+#' Oceanographic datasets provided by the Department of
+#' Fisheries and Oceans, Canada.
+#'
+#' @details
+#'
+#' `dfo` supplies functions for reading DFO data, returning
+#' objects that are based on the `oce` base class.  Because
+#' of this, the return values can be easily summarized, plotted,
+#' and otherwise manipulated easily with `oce` functions.
+#' This is illustrated within examples provided in
+#' the documentation for the various `dfo` functions.
+#'
+#' @docType package
+#' @name dfo
+NULL
+
