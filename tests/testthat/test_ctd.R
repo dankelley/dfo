86f3417688896eec342584d5ee93fc938aa019b5
dankelley <kelley.dan@gmail.com>
2021-02-08 10:43:17 -0400

    test read.ctd.ios() on both styles

--- tests/testthat/test_ctd.R
+++ tests/testthat/test_ctd.R
@@ -8,11 +8,13 @@ test_that("read.ctd.ios.netcdf()", {
   url <- "https://data.cioospacific.ca/erddap/files/IOS_CTD_Profiles/2007/2007-019-0055.ctd.nc"
   download.file(url, tempFile, mode="wb")
   ctd <- expect_silent(read.ctd.ios.netcdf(tempFile))
+  ctd2 <- expect_silent(read.ctd.ios(tempFile))
   unlink(tempFile)
 })
 
 test_that("read.ctd.ios.text()", {
   file <- system.file("extdata", "2007-019-055.ctd", package="dfo")
   ctd <- expect_silent(read.ctd.ios.text(file))
+  ctd2 <- expect_silent(read.ctd.ios(file))
 })
 
